b25664907d5bb08b95e30b0889e3bf98d6afab09
byykkonen <118409410+byykkonen@users.noreply.github.com>
2022-12-17 10:03:47 -0600

    Update assignment

--- app.js
+++ app.js
@@ -2,7 +2,7 @@
  *
  * @returns A promise that is designed to resolve with a list of hobbits, or potentially fail with an failure object. The failure object includes a boolean success property and a string message property.
  */
-async function getList() {
+function getList() {
   return new Promise((resolve, reject) => {
     setTimeout(() => {
       let potentialFail = Math.round(Math.random() * 100) < 10;
@@ -19,42 +19,30 @@ async function getList() {
 let feedbackP = document.querySelector("#error");
 let ul = document.querySelector("#list");
 
-let promise = getList();  //returns a promise
-
-// TODO: If the promise resolves with the list of hobbits
+// TODO: Handle the resolved or rejected states of the promise
+  getList() 
+    .then((list) =>  {
+      // TODO: If the promise resolves with the list of hobbits
 // Render the list of hobbits as list items within the unordered list with id="list" (check the index.html file)
 
-function handleList(list) {
-  list.forEach((hobbit) => {
-    let li = document.createElement("li");
-    li.textContent = hobbit;
-    ul.appendChild(li);
+      list.forEach((hobbit) => {
+      let li = document.createElement("li");
+      li.textContent = hobbit;
+     ul.appendChild(li);
   });
-}
-
-// TODO: If the promise rejects with the failure object
+})
+  .catch((err) => {
+    // TODO: If the promise rejects with the failure object
 // Display the failure message in the paragraph element with id="error" (check index.html file)
 
-function handleError(err) {
-  console.error(err);
-  feedbackP.textContent = err.message;
-}
-
-// TODO: Handle the resolved or rejected states of the promise
-
-async function updateDOMList() {
- try {
-  let list = await getList();
-  list.forEach((hobbit) => {
-    let li = document.createElement("li");
-    li.textContent = hobbit;
-    ul.appendChild(li);
+    errorPara.textContent = err.message;
   });
- } catch (err) {
-   console.error(err);
-   feedbackP.textContent = err.message;
- }
-}
+
+
+
+
+
+
 
 updateDOMList();
 
